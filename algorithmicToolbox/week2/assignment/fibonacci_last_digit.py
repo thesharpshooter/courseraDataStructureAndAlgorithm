996d8d0c45db277f953f1af3f6f1126bab77913f
thesharpshooter <prakash9266@gmail.com>
2017-05-11 02:20:54 +0530

    fibonacci last digit

--- algorithmicToolbox/week2/assignment/fibonacci_last_digit.py
+++ algorithmicToolbox/week2/assignment/fibonacci_last_digit.py
@@ -1,19 +1,12 @@
 # Uses python3
 import sys
 
-def get_fibonacci_last_digit_naive(n):
-    if n <= 1:
-        return n
+def get_fibonacci_last_digit(n):
+    fib = [0]*(n+1)
+    fib[1] = 1
+    for i in range(2,n+1):
+        fib[i] = (fib[i-1] + fib[i-2])%10
+    return fib[n]%10
+n = int(raw_input())
+print(get_fibonacci_last_digit(n))
 
-    previous = 0
-    current  = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-
-    return current % 10
-
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
