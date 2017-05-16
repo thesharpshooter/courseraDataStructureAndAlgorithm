320f41876bcac1a8a889f08d69729443aaa8498d
thesharpshooter <prakash9266@gmail.com>
2017-05-17 01:04:42 +0530

    first attemot

---
+++ algorithmicToolbox/week3/assignment/fractional_knapsack.py
@@ -0,0 +1,20 @@
+#pyton2
+import operator
+items,weight = map(int,raw_input().split(" "))
+item = []
+for i in range(items):
+    temp = map(int,raw_input().split(" "))
+    item.append((temp,1.0*temp[0]/temp[1]))
+item.sort(key=lambda x : x[1])
+total_value = 0
+for x in reversed(item):
+    if x[0][1] <= weight:
+        total_value += x[0][0]
+        weight -= x[0][1]
+    elif x[0][1] >weight:
+        total_value += x[1]*weight
+        weight = 0
+        break
+
+print total_value
+
