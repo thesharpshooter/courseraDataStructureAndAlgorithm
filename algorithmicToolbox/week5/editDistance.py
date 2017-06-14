e965cd9112c4b8b7faf355e9e28b4ab2605a2cca
thesharpshooter <prakash9266@gmail.com>
2017-06-15 02:42:57 +0530

    Edit Distance

---
+++ algorithmicToolbox/week5/editDistance.py
@@ -0,0 +1,26 @@
+x = raw_input()
+y = raw_input()
+m = len(x)
+n = len(y)
+
+def diff(i,j):
+    global x
+    global y
+    return int(x[i-1]!=y[j-1])
+def editDistance(x,y):
+    m = len(x)
+    n = len(y)
+    temp_x = [0]*(m+1)
+    distance = []
+    for i in range(n+1):
+        distance.append(temp_x[:])
+    for i in range(m+1):
+        distance[0][i] = i
+    for j in range(n+1):
+        distance[j][0] = j
+
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            distance[j][i] = min(1+distance[j-1][i],1+distance[j][i-1],diff(i,j)+distance[j-1][i-1])
+    return distance[n][m]
+print editDistance(x,y)
